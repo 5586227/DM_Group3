08ab09497ad0cf340229fcd89e6bb12aa8e24f31
jingu <2017413156@qq.com>
2024-03-17 18:31:11 +0000

    rename file datavalidation and schema, remove file load_data.R

--- R/load_data.R
+++
@@ -1,6 +0,0 @@
-library(readr)
-library(RSQLite)
-
-customers <- readr::read_csv('data_upload/customers.csv')
-my_connection <- RSQLite::dbConnect(RSQLite::SQLite(),'database/database.db')
-RSQLite::dbWriteTable(my_connection, 'customers', customers)
